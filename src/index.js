dbad968da7bfb99ef162ad584b44d3995c6aad27
Alex Mordue <alex.mordue@hotmail.com.au>
2019-03-11 17:45:26 +1100

    add hot reloading

--- src/index.js
+++ src/index.js
@@ -3,9 +3,25 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
+import { AppContainer } from "react-hot-loader";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const render = () => {
+  ReactDOM.render(
+    <AppContainer>
+      <App />
+    </AppContainer>,
+    document.getElementById("root")
+  );
+};
 
+render();
+
+// Webpack Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept("./components/App", () => {
+    render();
+  });
+}
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
